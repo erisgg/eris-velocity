d44324cfd89c9a7d314c0befbe4a390e040c304d
alfiejfs <alfie@alfiesmith.net>
2021-08-08 20:22:20 +0100

    Faster!

--- src/main/java/gg/eris/erisvelocity/ErisVelocity.java
+++ src/main/java/gg/eris/erisvelocity/ErisVelocity.java
@@ -71,7 +71,7 @@ public class ErisVelocity {
   @Subscribe
   public void onProxyInitialization(ProxyInitializeEvent event) {
     this.server.getScheduler().buildTask(this, () -> this.redisWrapper.set("playercount", this.mapper.createObjectNode()
-        .put("count", this.server.getPlayerCount()))).repeat(100, TimeUnit.MILLISECONDS)
+        .put("count", this.server.getPlayerCount()))).repeat(10, TimeUnit.MILLISECONDS)
         .schedule();
   }
 
